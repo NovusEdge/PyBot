b2872ad9faf35c96fbeb064ba5ef672049a4a795
NovusEdge <novusedge0@gmail.com>
2021-01-24 22:47:58 +0530

    Updated files

--- Bot/bot.py
+++ Bot/bot.py
@@ -1,10 +1,9 @@
-import discord, json, os, base64, pathlib
+import discord, json, os, base64, pathlib, asyncio
 from discord.ext import commands
-from BotCommands import bot_help, code_run
+from BotCommands import bot_help, code_run, get_quote, get_cat
 from BotCogs import *
 
 path = pathlib.Path(__file__).parent.absolute()
-print(path)
 os.chdir(path)
 
 botObj =  commands.Bot(command_prefix=">", help_command=None)
@@ -17,10 +16,14 @@ async def on_ready():
 async def help(ctx):
 	await bot_help.help(ctx)
 
-@botObj.event
-async def runcode(ctx, message):
-	await code_run.runcode(ctx, message)
-# https://discordpy.readthedocs.io/en/latest/api.html#discord.Message.add_reaction
+@botObj.command()
+async def quote(ctx):
+	await get_quote.quote(ctx)
+
+@botObj.command(aliases=["CATS"])
+async def cats(ctx):
+	await get_cat.cats(ctx)
+
 
 def run(bot, token):
     bot.run(token)
@@ -30,3 +33,15 @@ with open("../config", "r") as f:
 
 if __name__ == "__main__":
     run(botObj, TOKEN)
+
+
+# @botObj.event
+# async def on_message(ctx, message):
+# 	check = lambda reaction, user: user == message.author and str(reaction.emoji) == ':zap:'
+# 	try:
+#         reaction, user = await client.wait_for('reaction_add', timeout=60.0, check=check)
+#     except asyncio.TimeoutError:
+#         await channel.send('```txt\nTimed Out\n```')
+# 	else:
+# 		code_run.runcode(ctx, message)
+# https://discordpy.readthedocs.io/en/latest/api.html#discord.Message.add_reaction
